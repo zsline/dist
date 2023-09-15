d3d374799ce127033d49abb4169d50c0a6225b1b
zsline <sergei.dereka@gmail.com>
2023-09-15 16:29:06 +0300

    webemployee

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+
+// preloader
+window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+}
+// end preloader
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+gsap.to('.offer__title h2', {
+    scale: 1,
+    opacity: 1,
+    duration: 1
+});
+gsap.to('.offer__decor', {
+    rotate: 360,
+    duration: 40,
+    scale:1.6,
+});
+gsap.to('.botton-box', {
+    opacity: 1,
+    scale:1,
+    delay: 1.2,
+    duration: 0.4,
+});
+gsap.fromTo('.offer__wrapper', { opacity: 1 }, {
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.offer',
+        start: 100,
+        end: 650,
+        scrub: true
+    }
+});
+gsap.fromTo('.offer__img', { x:0 }, {
+    x: -40,
+    scrollTrigger: {
+        trigger: '.offer',
+        start: 0,
+        end: 650,
+        scrub: true
+    }
+});
+gsap.fromTo('.botton-box', { opacity:1 }, {
+    opacity:0,
+    scrollTrigger: {
+        trigger: '.offer',
+        start: 0,
+        end: 1350,
+        scrub: true
+    }
+});
+
+
+
+
+$( document ).ready(function() {
+    
+    // Function to change the nav-bar on scroll
+    $(window).scroll(function(){
+        ($(window).scrollTop() >= 100) ? (
+            $('.fixed-nav-bar').addClass('scrolled'),
+            $('.the-bass').addClass('scrolled')
+        ) : (
+            $('.fixed-nav-bar').removeClass('scrolled'),
+            $('.the-bass').removeClass('scrolled')
+        );
+    });
+    
+    // Drop Down Function
+    $('#menuButton').on('change', function(){
+        ($('#menuButton').is(':checked')) ? (
+            $('.the-bass').addClass('dropped')
+        ) : (
+            $('.the-bass').removeClass('dropped')
+        );
+    });
+    
+});
